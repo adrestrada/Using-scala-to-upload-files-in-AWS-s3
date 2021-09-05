478ce82af1b50757d22a97a26d3f3ddee5e8614c
ADRIANA ESTRADA <adr.estrada@gmail.com>
2021-09-05 09:45:01 -0400

    Update AmazonS3FileUploadBixi.scala

--- AmazonS3FileUploadBixi.scala
+++ AmazonS3FileUploadBixi.scala
@@ -7,20 +7,20 @@ import java.io.File
 
 object AmazonS3FileUpload extends App {
 
-  // specifying bucket name
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ specifying bucket name
   val bucketName = "data-bucket"
 
   //file to upload
   val fileToUpload = new File("***************")
 
-  //These Keys would be available to you in  "Security Credentials" of your Amazon S3 account
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~These Keys would be available to you in  "Security Credentials" of your Amazon S3 account
   val AWS_ACCESS_KEY = "***************"
   val AWS_SECRET_KEY = "**************"
 
   val yourAWSCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
   val amazonS3Client = new AmazonS3Client(yourAWSCredentials)
 
-  // This will create a bucket for storage
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~This will create a bucket for storage
   amazonS3Client.createBucket(bucketName)
   amazonS3Client.putObject(bucketName, "FileName", fileToUpload)
 }
